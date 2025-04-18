9a1456e48ceb6483c44324463605059e866e89d9
revalisa@revalisa <revalisa@revalisa>
2025-04-18 10:42:41 +0700

    binding databasse

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,30 @@
 interface Env {
 	ASSETS: Fetcher;
+	DB: D1Database;
 }
-
 export default {
-	fetch(request, env) {
+	async fetch(request: Request, env: Env) {
 		const url = new URL(request.url);
-		
-		if (url.pathname.startsWith("api/tasks")) {
+
+		if (url.pathname.startsWith("/api/tasks")) {
 			return Response.json([
 				{
 					id: '1',
 					title: "Task 1",
-					completed: false,
+					complated: false,
 					deadline: 1741247092,
 				},
 				{
 					id: '2',
 					title: "Task 2",
-					completed: true
+					complated: true,
 					deadline: 1741247092,
 				},
 			]);
-		} 
-		 return env.ASSETS.fetch(request);
-		},
-	} satisfies ExportedHandler<Env>;
-}
+			
+
+		}
+		return env.ASSETS.fetch(request);
+	},
+}	
+		
